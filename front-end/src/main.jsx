ef850ccad61354932033021821d39a2bfea0a433
SinfulBan1 <sinfulban@gmail.com>
2025-10-05 16:49:02 -0400

    secured firebase info

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -11,13 +11,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAut_I6Y2gJttKffnyZAAKDbfKPutV-McI",
-  authDomain: "learn-games-9813e.firebaseapp.com",
-  projectId: "learn-games-9813e",
-  storageBucket: "learn-games-9813e.firebasestorage.app",
-  messagingSenderId: "781883220653",
-  appId: "1:781883220653:web:4bd60802937101c876dc24",
-  measurementId: "G-32ZQGX54W3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
